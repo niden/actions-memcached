c7db6fb07b0b8161afac0d2598e3be04d0c0bd22
Nikolaos Dimopoulos <nikos@niden.net>
2019-12-12 23:38:22 -0500

    Trying something different

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/sh
+#!/bin/sh -l
 set -e
 
 # first arg is `-f` or `--some-option`
-if [ "${1#-}" != "$1" ]; then
-	set -- memcached "$@"
-fi
+#if [ "${1#-}" != "$1" ]; then
+#	set -- memcached "$@"
+#fi
 
-exec "$@"
+#exec "$@"
